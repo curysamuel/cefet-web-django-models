dea354203a89b9fbd75824260072eb6857a2c39d
Samuel Cury <cury.samuel@gmail.com>
2018-11-06 12:21:01 -0200

    first commit

---
+++ music/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class EstiloMusical(models.Model):
+	descrição = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.descrição
+
+
+class Musico(models.Model):
+	nome = models.CharField(max_length=1000)
+	email = models.EmailField(max_length=200)
+	idade = models.IntegerField()
+	ativo = models.BooleanField()
+
+	def __str__(self):
+		return self.nome
+
+
+
+class Banda(models.Model):
+	nome = models.CharField(max_length=1000)
+	email = models.EmailField(max_length=200)
+	estilo = models.ForeignKey(EstiloMusical, on_delete=models.PROTECT)
+	musicos = models.ManyToManyField(Musico, related_name='bandas')
+
+	def __str__(self):
+		return self.nome
+
+
+
